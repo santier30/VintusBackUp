f2af487b35993349a7afa533cff22f7e3d48420a
Santiago Hryb <santihryyb@gmail.com>
2023-11-01 10:29:45 -0300

    minor fix

--- server/controlers/usersControlers.js
+++ server/controlers/usersControlers.js
@@ -12,7 +12,57 @@ const validateMessage = (req , res , next) => {
         res.status(500).json({ message: "Fail sending message", err });
     }
     }
+    const sendMessage = async (req , res)=>{
+        const {name,email,phone,message} = req.body
+        console.log(req.body)
+    
+        try {
+         
+            const transporter = nodemailer.createTransport({
+                service: 'Gmail', 
+                auth: {
+                    user: 'VintusTm@gmail.com',
+                    pass: 'orao eoqs ooji qibw',
+                },
+            });
+    
+            
+            const corpMail = {
+                from: 'VintusTm@gmail.com',
+                to: 'VintusTm@gmail.com',
+                subject: `mensaje de ${name}`,
+                text: `
+                    Email: ${email}
+                    Phone: ${phone}
+                    Message: ${message}
+                `,
+            };
+    
+            const clientMail = {
+                from: 'VintusTm@gmail.com',
+                to: email,
+                subject: `Vintus servicio al cliente`,
+                text: `
+            Hola ${name} gracias por comunicarte con Vintus atencion al cliente
+            su mensaje esta siendo prosesado y sera respondido a la brevedad.
+    
+            VintusTm
+                `,
+            }
+    
+          
+            await transporter.sendMail(corpMail);
+            await transporter.sendMail(clientMail);
+    
+            res.status(200).json({ message: "Data posted successfully", name });
+        } catch (error) {
+            console.error(error);
+            res.status(500).json({ message: "Fail sending message", error: error.message });
+        }
+    
+    }
 
 module.exports ={
-    validateMessage
+    validateMessage,
+    sendMessage
 }
--- server/routes/users.js
+++ server/routes/users.js
@@ -3,59 +3,11 @@ const nodemailer = require('nodemailer');
 const controlers = require('../controlers/usersControlers')
 const router = express.Router();
 
-const {validateMessage} = controlers
+const {validateMessage,sendMessage} = controlers
 
 
 
-router.post('/Message',validateMessage, async (req , res)=>{
-    const {name,email,phone,message} = req.body
-    console.log(req.body)
-
-    try {
-     
-        const transporter = nodemailer.createTransport({
-            service: 'Gmail', 
-            auth: {
-                user: 'VintusTm@gmail.com',
-                pass: 'orao eoqs ooji qibw',
-            },
-        });
-
-        
-        const corpMail = {
-            from: 'VintusTm@gmail.com',
-            to: 'VintusTm@gmail.com',
-            subject: `mensaje de ${name}`,
-            text: `
-                Email: ${email}
-                Phone: ${phone}
-                Message: ${message}
-            `,
-        };
-
-        const clientMail = {
-            from: 'VintusTm@gmail.com',
-            to: email,
-            subject: `Vintus servicio al cliente`,
-            text: `
-        Hola ${name} gracias por comunicarte con Vintus atencion al cliente
-        su mensaje esta siendo prosesado y sera respuesto a la brevedad
-
-        VintusTm
-            `,
-        }
-
-      
-        await transporter.sendMail(corpMail);
-        await transporter.sendMail(clientMail);
-
-        res.status(200).json({ message: "Data posted successfully", name });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({ message: "Fail sending message", error: error.message });
-    }
-
-})
+router.post('/Message',validateMessage, sendMessage)
 
 
 
